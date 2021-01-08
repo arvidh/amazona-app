84fe0cae5a719b4960e65f87856d1b78c4f7284e
Arvid <arvid@oddbits.se>
2021-01-08 18:21:48 +0100

    Add backend and redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,23 +7,22 @@ import ProductScreen from './screens/ProductScreen'
 function App() {
   return (
     <BrowserRouter>
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a className="brand" href="/">amazona</a>
-        </div>
-        <div>
-          <a href="/cart">Cart</a>
-          <a href="/signin">Sign In</a>
-        </div>
-      </header>
-      <main>
-        <Route path="/product/:id" component={ProductScreen}></Route>
-        <Route exact path="/" component={HomeScreen}></Route>
-        
-      </main>
-      <footer className="row center">All right reserved</footer>
-    </div>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">amazona</a>
+          </div>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
+          </div>
+        </header>
+        <main>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route exact path="/" component={HomeScreen}></Route>
+        </main>
+        <footer className="row center">All right reserved</footer>
+      </div>
     </BrowserRouter>
   );
 }
